70999fe70638f824e6cf0a9fe9ae9238e46cf03d
James <jamesarnott2@gmail.com>
2022-06-24 23:18:30 +0100

    feat: added SENhistory to pupil

--- src/ATfile/ATFpupilData/Pupil/index.ts
+++ src/ATfile/ATFpupilData/Pupil/index.ts
@@ -8,6 +8,7 @@ import {
 import moment from 'moment';
 import { Phones, create as cPhones } from 'ATfile/ATFpupilData/Pupil/Phones';
 import { Admissions, create as cAdmissions } from 'ATfile/ATFpupilData/Pupil/Admissions';
+import { SENhistory, create as cSENhistory } from 'ATfile/ATFpupilData/Pupil/SENhistory';
 
 faker.locale = 'en_GB';
 
@@ -24,7 +25,7 @@ export type Pupil = {
   BasicDetails?: any;
   FSMhistory?: any;
   LookedAfter?: any;
-  SENhistory?: any;
+  SENhistory?: SENhistory;
   Admissions?: Admissions;
   Address?: Address;
   Phones?: Phones;
@@ -34,26 +35,30 @@ export type Pupil = {
   SuppInfo?: SuppInfo;
 };
 
-const create = (header: Header): Pupil => ({
-  ApplicationReference: createApplicationReference(header),
-  UPN: optionalRand(createUPN(header)), // TODO: make a UPN generator
-  UniqueLearnerNumber: optionalRand(createUniqueLearnerNumber()),
-  Forename: faker.name.firstName(),
-  Surname: faker.name.lastName(),
-  DOB: moment(faker.date.betweens(`${header.SourceSchool.AcademicYear - 3}-01-01`, `${header.SourceSchool.AcademicYear - 3}-12-31`)[0]).format('DD/MM/YYYY'),
-  Gender: Math.random() > 0.5 ? 'M' : 'F',
+const create = (header: Header): Pupil => {
+  const DOB = moment(faker.date.betweens(`${header.SourceSchool.AcademicYear - 3}-01-01`, `${header.SourceSchool.AcademicYear - 3}-12-31`)[0]).format('DD/MM/YYYY'),;
+  return {
+    ApplicationReference: createApplicationReference(header),
+    UPN: optionalRand(createUPN(header)), // TODO: make a UPN generator
+    UniqueLearnerNumber: optionalRand(createUniqueLearnerNumber()),
+    Forename: faker.name.firstName(),
+    Surname: faker.name.lastName(),
+    DOB,
+    Gender: Math.random() > 0.5 ? 'M' : 'F',
 
-  Admissions: optionalRand(cAdmissions()),
-  Address: optionalRand(generateAddress()),
-  Phones: optionalRand(cPhones()),
-  Email: optionalRand(faker.internet.email()),
-  /*
+		SENhistory: optionalRand(cSENhistory(DOB)),
+    Admissions: optionalRand(cAdmissions()),
+    Address: optionalRand(generateAddress()),
+    Phones: optionalRand(cPhones()),
+    Email: optionalRand(faker.internet.email()),
+    /*
     at Arcio, we're not lazy, we're progressive,
     so you can have many fathers, mothers and other contacts!
   */
-  Contacts: [...Array(Math.floor(Math.random() * 3) + 1)].map(() => cContact()),
+    Contacts: [...Array(Math.floor(Math.random() * 3) + 1)].map(() => cContact()),
 
-  SuppInfo: optionalRand(cSuppInfo()),
-});
+    SuppInfo: optionalRand(cSuppInfo()),
+  };
+};
 
 export { create };
