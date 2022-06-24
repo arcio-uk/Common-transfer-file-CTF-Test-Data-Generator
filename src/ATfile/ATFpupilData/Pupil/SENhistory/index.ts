3fbd8845937a8ba451d6335d1318e388cf6d7c54
James <jamesarnott2@gmail.com>
2022-06-24 23:31:43 +0100

    feat: added SENneeds

--- src/ATfile/ATFpupilData/Pupil/SENhistory/index.ts
+++ src/ATfile/ATFpupilData/Pupil/SENhistory/index.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { SuppInfo, createSuppInfo } from 'misc/misc';
+import { SuppInfo, createSuppInfo, optionalRand } from 'misc/misc';
 import moment from 'moment';
 
 const SENprovisions = ['N', 'E', 'K'];
@@ -10,7 +10,7 @@ type SENhistory = {
     SEN: {
       StartDate: string;
       SENprovision: string; // N, E or K
-    }
+    };
   }[];
   SENneeds?: {
     SENneed: {
@@ -18,23 +18,37 @@ type SENhistory = {
       NeedEndDate?: string;
       SENtypeRank: number; // 1 to 99, ranked needs
       SENtype: string;// any of the SEN types
-    }[];
+    };
   }[];
   SuppInfo?: SuppInfo;
 };
 
 const create = (DOB: string) => {
-  const SEN: SENhistory['SEN'] = [...Array(Math.floor(Math.random() * 3)+1).keys()].map(() => ({
+  const DOBdate = moment(DOB, 'DD/MM/YYYY').date();
+  const SEN: SENhistory['SEN'] = [...Array(Math.floor(Math.random() * 3) + 1).keys()].map(() => ({
     SEN: {
-      StartDate: moment(faker.date.between(moment(DOB, 'DD/MM/YYYY').date(), Date.now())).format('DD/MM/YY'),
+      StartDate: moment(faker.date.between(DOBdate, Date.now())).format('DD/MM/YY'),
       SENprovision: SENprovisions[Math.floor(Math.random() * SENprovisions.length)],
     },
   }));
 
-  
+  const SENneeds: SENhistory['SENneeds'] = [...Array(Math.floor(Math.random() * SENtypes.length - 1) + 1).keys()].map((index) => {
+    const maxStartDate = new Date();
+    const startDate = faker.date.between(DOBdate, maxStartDate.setMonth(maxStartDate.getMonth() - 1));
+    const endDate = faker.date.between(startDate, new Date());
+    return {
+      SENneed: {
+        NeedStartDate: optionalRand(moment(startDate).format('DD/MM/YYYY')),
+        NeedEndDate: optionalRand(moment(endDate)).format('DD/MM/YYYY'),
+        SENtypeRank: index,
+        SENtype: SENtypes[Math.floor(Math.random() * SENtypes.length)],
+      },
+    };
+  });
 
   return {
     SEN,
+    SENneeds: optionalRand(SENneeds),
     SuppInfo: createSuppInfo(),
   };
 };
