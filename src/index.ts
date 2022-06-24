f2053e969ce28f974bfe66b1dba1182e84b1db01
James <jamesarnott2@gmail.com>
2022-06-24 22:17:17 +0100

    lint: added spaces (auto)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { CreateSchoolJSON } from 'misc/misc';
-import {create as cAtFile} from 'ATfile';
+import { create as cAtFile } from 'ATfile';
 
 // inporting for efficiency
 import 'data/edubasealldata.json';
@@ -8,11 +8,11 @@ import 'data/edubasealldata.json';
 const init = async () => {
   // await CreateSchoolJSON(500);
   console.log('Generating!');
-  /*fs.writeFile(
+  /* fs.writeFile(
     'src/output.json',
     JSON.stringify(CreateHeader(), null, 2),
     () => console.log(CreateHeader()),
-  );*/
+  ); */
   console.log(JSON.stringify(cAtFile(), null, 2));
 };
 
