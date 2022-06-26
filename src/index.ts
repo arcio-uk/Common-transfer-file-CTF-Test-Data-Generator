296301e6c4ce69a2cdc9d3430f91c90b70ce8b73
James <jamesarnott2@gmail.com>
2022-06-26 13:17:05 +0100

    feat: index now creates and saves an XML file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { create as cAtFile } from 'ATfile';
-
+import { create } from 'ATfile/ATFpupilData/Pupil/SENhistory';
+import * as xmlToJSON from 'xml-js';
+import fs from 'fs';
 // inporting for efficiency
 import 'data/edubasealldata.json';
 
 const init = async () => {
-  // await CreateSchoolJSON(500);
   console.log('Generating!');
-  /* fs.writeFile(
-    'src/output.json',
-    JSON.stringify(CreateHeader(), null, 2),
-    () => console.log(CreateHeader()),
-  ); */
-  console.log(JSON.stringify(cAtFile(), null, 2));
+  const start = Date.now();
+  const jsObj = create('10/06/2002');
+
+  // converting to JSON before XML as the jsonify removes 'undefined' objects!
+  // TODO: replace this soon
+  const xml = xmlToJSON.json2xml(JSON.stringify(jsObj), { compact: true, ignoreComment: true, spaces: 4 });
+
+  fs.writeFile(
+    'src/output.xml',
+    xml,
+    () => console.log(xml),
+  );
+
+  console.log(`Running Time (Ms): ${Date.now() - start}`);
 };
 
 init();
