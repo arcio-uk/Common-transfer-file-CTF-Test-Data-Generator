b887c7d725b145b0ebb979099d59faed1302756a
James <jamesarnott2@gmail.com>
2022-06-28 19:31:20 +0100

    feat: added ATfile type

--- src/ATfile/index.ts
+++ src/ATfile/index.ts
@@ -1,7 +1,12 @@
-import { create as cHeader } from 'ATfile/Header';
-import { create as cATFpupilData } from 'ATfile/ATFpupilData';
+import { create as cHeader, Header } from 'ATfile/Header';
+import { ATFpupilData as ATFpupilDataType, create as cATFpupilData } from 'ATfile/ATFpupilData';
 
-const create = (PUPILS_TO_GENERATE: number) => {
+type ATfile = {
+  header: Header;
+  ATFpupilData: ATFpupilDataType;
+}
+
+const create = (PUPILS_TO_GENERATE: number): ATfile => {
   const header = cHeader();
   const ATFpupilData = cATFpupilData(header, PUPILS_TO_GENERATE);
   return {
